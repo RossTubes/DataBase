5f37a9c816116796058e2d6c2c180821c10f4d58
RossTubes <70896941+RossTubes@users.noreply.github.com>
2022-02-01 12:16:01 +0100

    nette versie

--- Program.cs
+++ Program.cs
@@ -9,25 +9,6 @@ namespace DataBase
             Functies functie = new Functies();
 
             functie.Start();
-            { 
-               /* foreach (Auto c in GroundLevel.GetGarage())
-                    {
-                        Console.WriteLine(c.GetName());
-                    }
-                Console.WriteLine("total Cars in Garage." + carCount2);
-
-                foreach (Auto c in FirstLevel.GetGarage())*/
-                {
-                    //Console.WriteLine(c.GetName());
-                }
-            }
         }
     }
 }
-
-
-
-       /*public string Name
-        {
-            get; private set;
-        }*/
